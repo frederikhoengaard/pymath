a5f13ab8ca19a582dc1332059319541e1b0bcb9d
Frederik Høngaard <mail@frederikhoengaard.com>
2020-11-08 23:38:45 +0100

    Create calculus.py

---
+++ src/calculus.py
@@ -0,0 +1,24 @@
+# calculus.py
+
+
+"""
+
+"""
+
+import fractions
+
+
+
+class Factor:
+    def __init__(self):
+        self.parts = []
+
+
+
+class Variable:
+    def __init__(self,name: str):
+        self.name = name
+        self.power = 1
+        self.factor = None
+
+
