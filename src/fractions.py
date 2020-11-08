bf895d44c5d743ff8b29b06faed1e0dcb3358de9
Frederik Høngaard <mail@frederikhoengaard.com>
2020-11-08 14:52:28 +0100

    added fractions.py

---
+++ src/fractions.py
@@ -0,0 +1,22 @@
+
+
+class Fraction:
+    def __init__(self,numerator,denominator):
+        if type(numerator) != 
+            raise ValueError('Numerator and denominator must be integer values!')
+
+        self.numerator = numerator
+        self.denominator = denominator
+
+
+
+
+
+
+
+
+
+a = Fraction(1,2)
+
+
+print(a.numerator)
